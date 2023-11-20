7cc0c9f73a770846ad872b39c65b9d122eafd74a
Swethaveeramani <112874390+Swethaveeramani@users.noreply.github.com>
2023-11-20 11:56:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux-state/CartState';
 import { ChakraProvider } from '@chakra-ui/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
